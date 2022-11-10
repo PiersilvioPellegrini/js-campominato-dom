ef6048c5e5cc6d6221df8436ac5c6fd46ed3133c
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2022-11-10 14:47:32 +0100

    Completed without COUNTER

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,95 @@
 // varibaile del bottone
 const btnPLay = document.querySelector(".btn-play");
+const difficoltà = document.querySelector('[name="difficoltà"]');
+let bombs;
+let counter;
 
 // evento sul bottone in cui richiamo la fuzione generaGrigliaCelle();
 btnPLay.addEventListener("click", function () {
- 
-  const difficoltà = document.querySelector('[name="difficoltà"]');
+
+  let cells;
 
   //   CONDIZIONE PER ASSEGNARE IL VALORE DELLE CELLE IN BASE ALLA DIFFICOLTÀ
   if (difficoltà.value === "Facile") {
     generaGrigliaCelle(10);
+    cells = 100;
   } else if (difficoltà.value === "Medio") {
     generaGrigliaCelle(9);
+    cells = 81;
   } else if (difficoltà.value === "Difficile") {
     generaGrigliaCelle(7);
+    cells = 49;
   }
+  bombs = generateBombList(cells, 16);
+  console.log(bombs);
 });
+
+/**
+ *
+ * @param {number} min
+ * @param {number} max
+ * @returns {number} - numero random
+ */
+
+// funzione che genera un numero random
+function generateRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+   * nella funzione passiamao come argomento il valore massimo che i numeri randomici possono assumere
+        
+    * @param {number} totalCells - numero di celle totali
+    * @param {Array} bombList - array contenente i numeri casuali delle bombe
+    * @return {Array} - array di numeri randomici
+   */
+
+function generateBombList(totalCells, numBomb) {
+  const bombList = [];
+
+  while (bombList.length < numBomb) {
+    const num = generateRandomNum(1, totalCells);
+    // se il numero non è presente nell'array lo inserisco
+    if (!bombList.includes(num)) {
+      bombList.push(num);
+    }
+  }
+  return bombList;
+}
+
+
+
+
+
+
+
+
+/**
+ * 
+ * 
+      
+  * @this {HTMLDivElement} - 
+ */
+
+function oncellCLick() {
+    
+  // creo una variabile a cui assegno il valore dell'elemento cliccato
+  const numInsideCell = +this.dataset.numeroCella;
+  // se il numero all'interno della cella è presente nell'array delle bombe
+  
+  if (bombs.includes(numInsideCell)) {
+    
+    // la partita finisce
+    this.classList.add("bg-red");
+    alert("Hai trovato una bomba dopo " +  "tentativi" );
+  }else{
+    this.classList.add("bg-celeste");
+    
+    
+  }
+  
+    
+}
 
 /**
  *
@@ -40,22 +115,20 @@ function generaGrigliaCelle(numCelleRiga) {
     cell.classList.add("cell");
     // aggiungo style inline
     cell.style.flexBasis = `calc(100% / ${numCelleRiga})`;
+
     // inserisco il contenuto nella cella (i)
-    cell.innerHTML = i + 1;
+    //cell.innerHTML = i + 1;
+
+    // creo un attributo personalizzato sull'elememto html
+    cell.dataset.numeroCella = i + 1;
+
     // padding inline
     container.style.padding = "80px 0";
 
     // creo evento sull'elemento cella sinìgola
-    cell.addEventListener("click", function () {
-      const numInsideCell = parseInt(this.textContent);
-      // inserisco la classe se non è presente e la rimuovo se è presente
-      cell.classList.toggle("bg-celeste");
-      // output console con il numero della cella cliccata
-      console.log("hai cliccato la cella numero: " + numInsideCell);
-
-      
-
-    });
+    cell.addEventListener("click", oncellCLick);
+    
+    
     // aggiungo la cella al contenitore
     container.appendChild(cell);
   }
